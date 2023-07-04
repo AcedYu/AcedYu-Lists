dcba04338ff8d81299f6634c2f1b6f073c5452e8
Alexander Yu <alexyu299@hotmail.com>
2023-07-04 00:41:07 +0000

    Lab Complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,6 @@
 
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Lists are ordered collections. This means that we could access elements at a certain position of a list,
@@ -29,7 +30,9 @@ public class Lab {
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        return null;
+        List<Integer> myList = new ArrayList();
+
+        return myList;
     }
 
     /**
@@ -39,7 +42,8 @@ public class Lab {
      * @return the size of List (number of items it holds.)
      */
     public int getSize(List<Integer> list){
-        return 0;
+        int size = list.size();
+        return size;
     }
 
     /**
@@ -51,6 +55,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
+        list.add(value);
     }
 
     /**
@@ -66,7 +71,7 @@ public class Lab {
      * @return the int at the location in 'list' represented by 'index'.
      */
     public int get(List<Integer> list, int index){
-        return 0;
+        return list.get(index);
     }
 
     /**
@@ -79,7 +84,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeFromList(List<Integer> list, int position){
-
+        list.remove(position);
     }
 
     /**
@@ -92,5 +97,6 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void updateAtPosition(List<Integer> list, int position, int value){
+        list.set(position, value);
     }
 }
